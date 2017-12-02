17c3841650d450009ca0d3766a0f4965e7453f5a
c-weidinger <mr17m016@technikum-wien.at>
2017-12-02 14:39:19 +0100

    Clion Project created
    Included Raphaels code in CLion Project

---
+++ Source/cmake-build-debug/CMakeFiles/Source.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Source.dir/main.cpp.o"
+  "Source.pdb"
+  "Source"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Source/main.cpp
+++ Source/main.cpp
@@ -9,38 +9,38 @@
 int main()
 {
 
-	int cols=20, rows=20;
-	//some basic part stuff
-	vector<Part> myFirstPuzzle;
-	Part myFirstPart;
-	myFirstPart.setConnections(0b00101000);
-	myFirstPuzzle.push_back(myFirstPart);
+    int cols=20, rows=20;
+    //some basic part stuff
+    vector<Part> myFirstPuzzle;
+    Part myFirstPart;
+    myFirstPart.setConnections(0b00101000);
+    myFirstPuzzle.push_back(myFirstPart);
 
-	//some basic random puzzle stuff
-	randomBox myRandomBox(cols,rows);
-	myRandomBox.createRandomPuzzle();
-	vector<PuzzlePiece> myFirstBox = myRandomBox.shuffle();
+    //some basic random puzzle stuff
+    randomBox myRandomBox(cols,rows);
+    myRandomBox.createRandomPuzzle();
+    vector<PuzzlePiece> myFirstBox = myRandomBox.shuffle();
 
-	//undo everything and make this puzzle fucking imba hard!!!
-	//need 40x40 for this, so check your status
+    //undo everything and make this puzzle fucking imba hard!!!
+    //need 40x40 for this, so check your status
 
-	//makehard4040puzzle(myFirstBox);
+    //makehard4040puzzle(myFirstBox);
 
-	//some advanced solver stuff
-	vector<LogEntry> log;
-	vector<PuzzlePiece*> p_myFirstBox;
+    //some advanced solver stuff
+    vector<LogEntry> log;
+    vector<PuzzlePiece*> p_myFirstBox;
 
-	//BoxClassify myFirstBox();
+    //BoxClassify myFirstBox();
 
-	cout << "original puzzle: " << endl;
-	myRandomBox.printPuzzle();
-	cout << endl;
-	for(int i=0;i<myFirstBox.size();i++)
-		p_myFirstBox.push_back(&myFirstBox[i]);
-	Puzzle puzzleMat(cols, rows);
+    cout << "original puzzle: " << endl;
+    myRandomBox.printPuzzle();
+    cout << endl;
+    for(int i=0;i<myFirstBox.size();i++)
+        p_myFirstBox.push_back(&myFirstBox[i]);
+    Puzzle puzzleMat(cols, rows);
 
-	//vector<vector<PuzzlePiece*>> ab1class = abstractionLayer1classify(log, p_myFirstBox,puzzleMat);
-	while(next(log, p_myFirstBox,puzzleMat));
+    //vector<vector<PuzzlePiece*>> ab1class = abstractionLayer1classify(log, p_myFirstBox,puzzleMat);
+    while(next(log, p_myFirstBox,puzzleMat));
 
-	puzzleMat.printPuzzle();
+    puzzleMat.printPuzzle();
 }
